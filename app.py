55a93e011b1433adadd5bef85ef16df4ef7f5fec
Harsh Gupta <91787765+DevilsN3st@users.noreply.github.com>
2022-10-04 13:03:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+@app.route('/', methods=["GET", "POST"])
+
+def index():
+
+    url = "https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content,"html.parser")
+    outerdata = soup.find_all('div', class_='widget-listing', limit=5)
+    finalNews = ""
+
+    for data in outerdata :
+        news  = data.div.div.a["title"]
+        finalNews += "\u2022 " + news + "\n"
+
+    print(finalNews)
+    return render_template("index.html", News = finalNews)
+
+
